d86b98c35264457d98b85cc83e8244e05d9e5f5f
Djordi <d.janssen@student.utwente.nl>
2020-04-01 17:56:50 +0200

    Grading update

--- src/main/java/nl/utwente/soa/digital/testing/grading/GradingApplication.java
+++ src/main/java/nl/utwente/soa/digital/testing/grading/GradingApplication.java
@@ -2,8 +2,10 @@ package nl.utwente.soa.digital.testing.grading;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.jms.annotation.EnableJms;
 
 @SpringBootApplication
+@EnableJms
 public class GradingApplication {
 
 	public static void main(String[] args) {
