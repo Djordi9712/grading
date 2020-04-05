e2f14c6d0e32ad21b64d974fc8746f8824bd070e
Djordi <d.janssen@student.utwente.nl>
2020-04-06 00:27:32 +0200

    Messagequeue receive exam from examination service

--- src/main/java/nl/utwente/soa/digital/testing/grading/ResultReceiver.java
+++ src/main/java/nl/utwente/soa/digital/testing/grading/ResultReceiver.java
@@ -1,41 +1,17 @@
 package nl.utwente.soa.digital.testing.grading;
-import org.apache.activemq.command.ActiveMQMessage;
-import org.jline.terminal.Terminal;
-import org.slf4j.Logger;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.jms.annotation.JmsListener;
-import org.springframework.jms.core.JmsTemplate;
-import org.springframework.stereotype.Component;
+import integration.Examination;
 
-import javax.jms.JMSException;
+import org.springframework.jms.annotation.JmsListener;
+import org.springframework.stereotype.Component;
 
 @Component
 public class ResultReceiver {
 
-    @Autowired
-    private Terminal terminal;
-
-    @JmsListener(destination = "${queue.ActiveMQ.DLQ}")
-    public void printError() {
-        terminal.writer().println();
-        terminal.writer().println("Could not calculate the equation");
-        terminal.writer().println();
-        terminal.writer().flush();
+    @JmsListener(destination = "${queue.exam}")
+    public void receiveMessage(Examination examination) {
+        for (Examination.Answers.Entry answers : examination.getAnswers().getEntry()) {
+            System.out.println("Key: " + answers.getKey());
+            System.out.println("Value: " + answers.getValue());
+        }
     }
-
-
-//    @JmsListener(destination = "${queue.results}")
-//    public void receiveMessage(CalculatorResponse calculatorResponse) {
-//
-//        terminal.writer().println();
-//        terminal.writer().write(calculatorResponse.getCalculationString());
-//        terminal.writer().println();
-//        terminal.writer().flush();
-//}
-
-
-
-
-
 }
